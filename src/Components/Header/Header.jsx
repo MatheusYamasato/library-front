194c9b9b248326dd1a8de117305446c6e21c40a5
MatheusYamasato <matheus-yamasato@hotmail.com>
2021-05-26 14:55:59 -0300

    atualizando

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './Header.module.css'
 import { Link } from 'react-router-dom'
+import logo from '../../imagens/logo.png'
 
 export const Header = () => {
     return (
         <header className={style.header}>
             <nav className={style.nav}>
+                <Link to="/"><img src={logo} alt="Logo da livraria"/></Link>
                 <Link className={style.link} to="/livros">Livros</Link>
                 <Link className={style.link} to="/usuarios">Usuários</Link>
                 <Link className={style.link} to="/operadores">Operadores</Link>
